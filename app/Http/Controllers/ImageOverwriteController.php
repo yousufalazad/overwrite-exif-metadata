f9899326f07d4a41be6e429b127aede1a6e3f6e0
Yousuf Al Azad <yousufalazad@gmail.com>
2025-04-15 13:12:03 +0100

    Initial commit

---
+++ app/Http/Controllers/ImageOverwriteController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ImageOverwrite;
+use Illuminate\Http\Request;
+
+class ImageOverwriteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ImageOverwrite $imageOverwrite)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ImageOverwrite $imageOverwrite)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ImageOverwrite $imageOverwrite)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ImageOverwrite $imageOverwrite)
+    {
+        //
+    }
+}
